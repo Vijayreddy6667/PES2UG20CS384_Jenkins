ca8eabac0a4e12daa32789d14f1a9052bb8db8bd
Vijayreddy6667 <118299761+Vijayreddy6667@users.noreply.github.com>
2023-02-19 10:49:24 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, Humans\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and running\n";
   return 0;
 }
